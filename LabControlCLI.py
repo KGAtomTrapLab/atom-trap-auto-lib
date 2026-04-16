a704e2e6c54fb0ce945f978057296e6384370602
rockwoodfrank <rockwoodfrank@gmail.com>
2026-04-16 14:50:08 -0700

    Minor changes

--- LabControlCLI.py
+++ LabControlCLI.py
@@ -138,18 +138,36 @@ class LabControlCLI(cmd2.Cmd):
         
 
     pot_parser = cmd2.Cmd2ArgumentParser()
-    pot_parser.add_argument("wiper", type=int)
+    pot_parser.add_argument("--wiper", type=int)
 
     @cmd2.with_argparser(pot_parser)
     def do_pot(self, args):
         # TODO: Implement
         'Set the wiper position of the ramp  (0-127)'
 
-    def do_set_current(self, arg):
-        'Set the current of the laser: set_current <current>'
-        # laser_controller = LaserController()
-        # laser_controller.set_current(float(arg))
-        print("Not actually setting the current just printing")
+
+    laser_parser = cmd2.Cmd2ArgumentParser()
+    laser_parser.add_argument('state', choices=['on', 'off'], help='Turn laser on or off')
+
+    @cmd2.with_argparser(laser_parser)
+    def do_laser(self, args):
+        'Toggle the laser'
+        if args.state == "on":
+            self.control_calibrator.laser_controller.laser_on()
+            self.control_calibrator.laser_controller.tec_on()
+        elif args.state == "off":
+            self.control_calibrator.laser_controller.laser_off(
+            self.control_calibrator.laser_controller.tec_off()
+        else: self.perror("Provide a --pwr setting.")
+    
+
+    # def do_set(self, args):
+    #     'Set the current of the laser: set_current <current>'
+    #     # laser_controller = LaserController()
+    #     # laser_controller.set_current(float(arg))
+    #     print("Not actually setting the current just printing")
+
+    # def do_get(self, args):
 
     # def do_get_current(self, arg):
     #     'Get the current of the laser: get_current'
--- main.py
+++ main.py
@@ -4,12 +4,6 @@ from log_data import DataLogger
 from LabControlCLI import LabControlCLI
 
 if __name__ == "__main__":
-    # data_logger = DataLogger("./logs")
-    # control_calibrator = LaserControlCalibrator()
-    # control_calibrator.connect_laser_controller()
-    # digital_davll = Digital_DAVLL(data_logger)
-    # digital_davll.connect()
-
     # control_calibrator.gather_data(12950, 14500, 100, 80, 120, 1, digital_davll, data_logger)
     # while True:
     #     print(digital_davll.get_data_line())
