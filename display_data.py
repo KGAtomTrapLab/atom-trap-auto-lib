f9ae83b13a60b16ac7d63dde955e1813c0935334
DrErker <rockwoodfrank@gmail.com>
2026-04-29 16:32:35 -0700

    Add fixes to prevent graph freezing

--- display_data.py
+++ display_data.py
@@ -12,7 +12,7 @@ class Data_Graph():
 
     def create_graph(self):
         self.fig, self.ax = plt.subplots()
-        self.ax.set_ylim(-2048, 2048)
+        self.ax.set_ylim(-1, 2048)
         self.line1, = self.ax.plot(self.data)
         self.line2, = self.ax.plot(self.data)
         plt.show()
